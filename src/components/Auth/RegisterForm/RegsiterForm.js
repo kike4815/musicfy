3c3201574fac0716bee93ca3c891b4f71e491f37
kike4815 <e.pedros.sanchez@gmail.com>
2020-09-10 07:40:25 +0200

    add validation pattern and styled RegisterComponent

--- src/components/Auth/RegisterForm/RegsiterForm.js
+++ src/components/Auth/RegisterForm/RegsiterForm.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button, Icon, Input } from "semantic-ui-react";
+import { validateEmail } from "../../../utils/validation";
 import firebase from "../../../utils/firebase";
 import "firebase/auth";
 
@@ -7,30 +8,95 @@ import "./RegisterForm.scss";
 
 export default function RegsiterForm(props) {
   const { setSelectedForm } = props;
+  const [formData, setFormData] = useState(defautlValuesForm());
+  const [showPassword, setShowPassword] = useState(false);
+  const [formError, setFormError] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   const onSubmit = () => {
-    console.log("Formulario enviado...");
+    setFormError({});
+    let errors = {};
+    let formOk = true;
+
+    if (!validateEmail(formData.email)) {
+      errors.email = true;
+      formOk = false;
+    }
+    if (formData.password.length < 6) {
+      errors.password = true;
+      formOk = false;
+    }
+    if (!formData.username) {
+      errors.username = true;
+      formOk = false;
+    }
+    setFormError(errors);
+
+    if (formOk) {
+      console.log("formulario Enviado");
+    }
+  };
+
+  function defautlValuesForm() {
+    return {
+      email: "",
+      password: "",
+      username: ""
+    };
+  }
+
+  const onChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
   };
 
   return (
     <div className="register-form">
       <h1>Empieza a escuchar con una cuenta de Musicfy gratis</h1>
-      <Form onSubmit={onsubmit}>
+      <Form onSubmit={onSubmit} onChange={onChange}>
         <Form.Field>
           <Input
             type="text"
             name="email"
             placeholder="correo electrónico"
             icon="mail outline"
+            error={formError.email}
           />
+          {formError.email && (
+            <span className="error-text">
+              Por favor, introduce un email válido
+            </span>
+          )}
         </Form.Field>
         <Form.Field>
           <Input
-            type="password"
+            type={showPassword ? "text" : "password"}
             name="password"
             placeholder="Contraseña"
-            icon="eye"
+            error={formError.password}
+            icon={
+              showPassword ? (
+                <Icon
+                  name="eye slash outline"
+                  link
+                  onClick={handleShowPassword}
+                />
+              ) : (
+                <Icon name="eye" link onClick={handleShowPassword} />
+              )
+            }
           />
+          {formError.password && (
+            <span className="error-text">
+              Por favor, introduce un password de longitud igual o superior a 6
+            </span>
+          )}
         </Form.Field>
         <Form.Field>
           <Input
@@ -38,7 +104,11 @@ export default function RegsiterForm(props) {
             name="username"
             placeholder="nombre que desees poner"
             icon="user circle outline"
+            error={formError.username}
           />
+          {formError.username && (
+            <span className="error-text">Por favor, introduce un usuario</span>
+          )}
         </Form.Field>
         <Button type="submit">Continuar</Button>
       </Form>
