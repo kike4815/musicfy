795f3ee1839963d08e5414a90249c00c82db3952
kike4815 <e.pedros.sanchez@gmail.com>
2020-09-13 20:34:39 +0200

    add MenuLeft v.2 & add Basic Modal for artsit or song

--- src/components/MenuLeft/MenuLeft.js
+++ src/components/MenuLeft/MenuLeft.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Menu, Icon } from "semantic-ui-react";
 import { Link, withRouter } from "react-router-dom";
 import { isUserAdmin } from "../../utils/api";
+import BasicModal from "../Modal/BasicModal/BasicModal";
 
 import "./MenuLeft.scss";
 
@@ -9,6 +10,9 @@ function MenuLeft(props) {
   const { user, location } = props;
   const [activeMenu, setActiveMenu] = useState(location.pathname);
   const [userAdmin, setUserAdmin] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [titleModal, setTitleModal] = useState(null);
+  const [contentModal, setContentModal] = useState(null);
 
   useEffect(() => {
     isUserAdmin(user.uid).then((response) => {
@@ -20,41 +24,66 @@ function MenuLeft(props) {
     setActiveMenu(menu.to);
   };
 
+  const handleModal = (type) => {
+    switch (type) {
+      case "artist":
+        setTitleModal("Nuevo Artista");
+        setContentModal(<h2>Formulario nuevo artista</h2>);
+        setShowModal(true);
+        break;
+      case "song":
+        setTitleModal("Nueva Canción");
+        setContentModal(<h2>Formulario nueva canción</h2>);
+        setShowModal(true);
+        break;
+      default:
+        setTitleModal(null);
+        setContentModal(null);
+        setShowModal(false);
+        break;
+    }
+  };
+
   return (
-    <Menu className="menu-left" vertical>
-      <div className="top">
-        <Menu.Item
-          as={Link}
-          to="/"
-          name="home"
-          active={activeMenu === "/"}
-          onClick={handleMenu}
-        >
-          <Icon name="home" /> Inicio
-        </Menu.Item>
-        <Menu.Item
-          as={Link}
-          to="/artists"
-          name="artists"
-          active={activeMenu === "/artists"}
-          onClick={handleMenu}
-        >
-          <Icon name="music" /> Artistas
-        </Menu.Item>
-      </div>
-      {userAdmin && (
-        <div className="footer">
-          <Menu.Item>
-            <Icon name="plus square outline" />
-            Nuevo Artista
+    <>
+      <Menu className="menu-left" vertical>
+        <div className="top">
+          <Menu.Item
+            as={Link}
+            to="/"
+            name="home"
+            active={activeMenu === "/"}
+            onClick={handleMenu}
+          >
+            <Icon name="home" /> Inicio
           </Menu.Item>
-          <Menu.Item>
-            <Icon name="plus square outline" />
-            Nueva Canción
+          <Menu.Item
+            as={Link}
+            to="/artists"
+            name="artists"
+            active={activeMenu === "/artists"}
+            onClick={handleMenu}
+          >
+            <Icon name="music" /> Artistas
           </Menu.Item>
         </div>
-      )}
-    </Menu>
+        {userAdmin && (
+          <div className="footer">
+            <Menu.Item onClick={() => handleModal("artist")}>
+              <Icon name="plus square outline" />
+              Nuevo Artista
+            </Menu.Item>
+            <Menu.Item onClick={() => handleModal("song")}>
+              <Icon name="plus square outline" />
+              Nueva Canción
+            </Menu.Item>
+          </div>
+        )}
+      </Menu>
+      <BasicModal show={showModal} setShow={setShowModal} title={titleModal}>
+        {contentModal}
+      </BasicModal>
+    </>
   );
 }
 export default withRouter(MenuLeft);
