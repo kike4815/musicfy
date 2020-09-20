f0d6b3072d951f5826a64de40c905a67f1f10782
kike4815 <e.pedros.sanchez@gmail.com>
2020-09-20 19:40:58 +0200

    end Artist Page and start with React Player

--- src/pages/Artist/Artist.js
+++ src/pages/Artist/Artist.js
@@ -3,7 +3,8 @@ import { withRouter } from "react-router-dom";
 import firebase from "../../utils/firebase";
 import "firebase/firestore";
 import BannerArtist from "../../components/BannerArtist/BannerArtist";
-
+import { map } from "lodash";
+import BasicSliderItems from "../../components/Sliders/BasicSliderItems";
 import "./Artist.scss";
 
 const db = firebase.firestore(firebase);
@@ -11,20 +12,47 @@ const db = firebase.firestore(firebase);
 function Artist(props) {
   const { match } = props;
   const [artist, setArtist] = useState(null);
+  const [album, setAlbum] = useState([]);
 
   useEffect(() => {
     db.collection("artists")
       .doc(match?.params?.id)
       .get()
       .then((response) => {
-        setArtist(response.data());
+        const data = response.data();
+        data.id = response.id;
+        setArtist(data);
       });
   }, [match]);
+
+  useEffect(() => {
+    if (artist) {
+      db.collection("album")
+        .where("artist", "==", artist.id)
+        .get()
+        .then((response) => {
+          const arrayAlbums = [];
+          map(response?.docs, (album) => {
+            const data = album.data();
+            data.id = album.id;
+            arrayAlbums.push(data);
+          });
+          setAlbum(arrayAlbums);
+        });
+    }
+  }, [artist]);
 
   return (
     <div className="artist">
       {artist && <BannerArtist artist={artist} />}
-      <h2>Más info...</h2>
+      <div className="artist__content">
+        <BasicSliderItems
+          title="Álbumes"
+          data={album}
+          folderImage="album"
+          urlName="album"
+        />
+      </div>
     </div>
   );
 }
